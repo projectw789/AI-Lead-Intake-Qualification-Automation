39ea75906b26787c806b7d66013e0d66a99a54ce
projectw789 <projectw789@gmail.com>
2026-09-15 23:54:18 +0100

    fixed import issue

--- src/aliqa/main.py
+++ src/aliqa/main.py
@@ -1,11 +1,13 @@
 from .user_input import first_user_query_input, second_user_query_input
 from .ai_client import llm_first_query_validation
-from .second_query_decision import decision
+from .second_query_decision import decision_one
 
 first_query = first_user_query_input()
 
 first_ai_response = llm_first_query_validation(first_query)
 
-decision_output = decision(first_ai_response, first_query)
+decision_one_output = decision_one(first_ai_response, first_query)
 
-print(decision_output)
+print(decision_one_output)
+
+# uv run python -m aliqa.main
