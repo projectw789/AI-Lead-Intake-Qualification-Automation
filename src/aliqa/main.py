e2ed911043c917a115da1d163e7bef6512bb4338
projectw789 <projectw789@gmail.com>
2026-09-18 17:04:49 +0100

    added if statement to ahndle diff final_query_obj diff data types

--- src/aliqa/main.py
+++ src/aliqa/main.py
@@ -3,7 +3,7 @@ from .ai_client import llm_first_query_validation
 from .decisions import decision_one, decision_two
 from .lead_qualification import customer_profile_creation, customer_priority_qualification
 from .sql_database import create_database, save_to_database, display_database
-
+from .pydantic_models import FirstClientQuery, SecondClientQuery
 
 
 create_database()
@@ -14,16 +14,18 @@ first_ai_response = llm_first_query_validation(first_query)
 
 final_query_object = decision_one(first_ai_response, first_query)
 
+if isinstance(final_query_object,FirstClientQuery) or isinstance(final_query_object, SecondClientQuery):
+    value, urgency = decision_two(final_query_object)
 
-value, urgency = decision_two(final_query_object)
+    customer_obj = customer_profile_creation(final_query_object, value, urgency)
 
-customer_obj = customer_profile_creation(final_query_object, value, urgency)
+    upd_customer_obj = customer_priority_qualification(customer_obj)
 
-upd_customer_obj = customer_priority_qualification(customer_obj)
+    save_to_database(upd_customer_obj)
 
-save_to_database(upd_customer_obj)
-
-display_func_output = display_database()
-print(display_func_output)
+    display_func_output = display_database()
+    print(display_func_output)
+else:
+    print("temp")
 
 # uv run python -m aliqa.main
